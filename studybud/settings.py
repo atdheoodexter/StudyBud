ee7d1f117accb1648d847eeba146135a988ce1aa
atdheoo <atdhe.muharremi@gmail.com>
2025-03-24 02:14:30 +0100

    update home and profile html files

--- studybud/settings.py
+++ studybud/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -107,11 +108,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'  # This should already be there
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, "static"),  # This is for development use
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # This is needed for collectstatic
 
 # STATIC_ROOT =
 
