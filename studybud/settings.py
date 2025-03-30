8a08d7f6346ef73a9a20ba504664299d6f96c3c7
atdheoo <atdhe.muharremi@gmail.com>
2025-03-30 09:52:17 +0200

    docker compose file for runing my app

--- studybud/settings.py
+++ studybud/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'studybud',  # PostgreSQL database name
+        'USER': 'myuser',  # PostgreSQL username
+        'PASSWORD': 'mypassword',  # PostgreSQL password
+        'HOST': 'my_postgres',  # PostgreSQL container name (match with Docker Compose)
+        'PORT': '5432',  # PostgreSQL port
     }
 }
 
